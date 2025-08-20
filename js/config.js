0fa5fb5b6d71cd50c5aab25f046078aadf2af964
creazl <3654162552@qq.com>
2025-08-20 21:24:08 +0800

    Update config.js
    新增三个API源

--- js/config.js
+++ js/config.js
@@ -115,8 +115,18 @@ const API_SITES = {
         name: '量子资源站'
     },
     testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
+        api: 'https://www.fantuan.tv/api.php/provide/vod/',
+        name: '饭团影视',
+        adult: true
+    },
+    testSource: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod/',
+        name: '影视工厂',
+        adult: true
+    },
+    testSource: {
+        api: 'https://www.qiqidys.com/api.php/provide/vod/',
+        name: '七七资源',
         adult: true
     },
 };
